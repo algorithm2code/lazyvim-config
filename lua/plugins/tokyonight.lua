26ff249e4887cdf84c602e5ffc634b114a9cc722
sajidCode-gh <muhammadsajidkhan616@gmail.com>
2024-04-01 03:23:18 +0500

    first commit

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,12 @@
+return {
+  "folke/tokyonight.nvim",
+  opts = {
+    transparent = true,
+    prioriy = 9999,
+    style = "night",
+    styles = {
+      sidebars = "transparent",
+      floats = "transparent",
+    },
+  },
+}
